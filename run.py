28888554c98f2748c03b14aa823d52650063123d
cuducos <cuducos@gmail.com>
2014-03-28 23:29:25 +0000

    Added logging in debug mode

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 #!venv/bin/python
 from app import app
-app.run(debug=True)
+app.debug = True
+if not app.debug:
+    import logging
+    from logging.handlers import FileHandler
+    file_handler = FileHandler('log.txt')
+    file_handler.setLevel(logging.INFO)
+app.run()
